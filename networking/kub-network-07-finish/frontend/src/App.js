eb273cec6faa94319281bad4393fd8ede9dc2c94
Juyel Rana <juyel@enzan-k.com>
2023-01-27 11:01:16 +0900

    Proxy pass reverse proxy added

---
+++ networking/kub-network-07-finish/frontend/src/App.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect, useCallback} from 'react';
+
+import './App.css';
+import TaskList from './components/TaskList';
+import NewTask from './components/NewTask';
+
+function App() {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = useCallback(function () {
+        fetch(`/api/tasks`, {
+            headers: {
+                'Authorization': 'Bearer abc'
+            }
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (jsonData) {
+                console.log(jsonData);
+                setTasks(jsonData.tasks);
+            }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    useEffect(
+        function () {
+            fetchTasks();
+        },
+        [fetchTasks]
+    );
+
+    function addTaskHandler(task) {
+        fetch(`/api/tasks`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer abc',
+            },
+            body: JSON.stringify(task),
+        })
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (resData) {
+                console.log(resData);
+            }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    return (
+        <div className='App'>
+            <section>
+                <NewTask onAddTask={addTaskHandler}/>
+            </section>
+            <section>
+                <button onClick={fetchTasks}>Fetch Tasks</button>
+                <TaskList tasks={tasks}/>
+            </section>
+        </div>
+    );
+}
+
+export default App;
